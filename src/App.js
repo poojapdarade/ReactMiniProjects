86518fcc01a6ecf55a60f879b444431b79a0e041
poojapdarade <poojap.darade@gmail.com>
2024-05-31 23:40:41 +0100

    Stopwatch mini porject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const { time, setTime } = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Stopwatch</h1>
+      <div>
+        <span>{"0" + Math.floor((time / 60000) % 60)}:</span>
+        <span>{"0" + Math.floor((time / 1000) % 60)}:</span>
+        <span>{"0" + Math.floor((time / 10) % 100)}:</span>
+      </div>
+    </>
   );
 }
 
